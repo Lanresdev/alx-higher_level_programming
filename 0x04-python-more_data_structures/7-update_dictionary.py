32f442a55d92f7c9b29d65639ff885e336a7408c
Lanresdev <lanresdev18@gmail.com>
2023-06-16 14:04:30 -0700

    pymordat

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or add key or value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
